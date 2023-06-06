1d8139508cffa0887af44ae094b001f39a3f191d
Nicolas Oviedo <nicoohdeoviedo@gmail.com>
2023-06-06 16:39:01 -0300

    script nuevamente

--- src/script/main.js
+++ src/script/main.js
@@ -60,52 +60,91 @@ const btn_menuCerrar = () => {
   navbar.classList.remove("active");
 };
 
+// function cambiarSeccion(seccionId) {
+//   let seccion = document.getElementById(seccionId);
+//   if (seccion) {
+//     seccion.scrollIntoView({ behavior: "smooth" });
+//     history.replaceState({ seccionId: seccionId }, "", "#" + seccionId);
+//   }
+// }
+
+// function handleClick(event) {
+//   event.preventDefault();
+//   let seccionId = event.target.getAttribute("href").substring(1);
+//   cambiarSeccion(seccionId);
+// }
+
+// let enlacesNavegacion = document.querySelectorAll('a[href^="#"]');
+// for (let i = 0; i < enlacesNavegacion.length; i++) {
+//   enlacesNavegacion[i].addEventListener("click", handleClick);
+// }
+
+// window.addEventListener("popstate", function (event) {
+//   if (event.state && event.state.seccionId) {
+//     cambiarSeccion(event.state.seccionId);
+//   }
+// });
+
+// // Deshabilitar la restauración automática de desplazamiento
+// if ("scrollRestoration" in history) {
+//   history.scrollRestoration = "manual";
+// }
+
+// // Función para manejar el evento de antes de descargar la página
+// window.addEventListener("beforeunload", function () {
+//   // Volver a la parte superior de la página al salir
+//   window.scrollTo({ top: 0 });
+// });
+
+// // Función para manejar el evento de clic en el botón de atrás del navegador
+// window.addEventListener("popstate", function (event) {
+//     if (event.state && event.state.seccionId) {
+//       var seccionActual = document.getElementById(event.state.seccionId);
+//       if (seccionActual && window.pageYOffset > 0) {
+//         // Si no está en la parte superior de la página, desplázate hacia arriba
+//         window.scrollTo({ top: 0, behavior: "smooth" });
+//       } else {
+//         // Si está en la parte superior de la página, vuelve atrás en el historial
+//         window.history.back();
+//       }
+//     }
+//   });
+
 function cambiarSeccion(seccionId) {
-  let seccion = document.getElementById(seccionId);
-  if (seccion) {
-    seccion.scrollIntoView({ behavior: "smooth" });
-    history.replaceState({ seccionId: seccionId }, "", "#" + seccionId);
+    let seccion = document.getElementById(seccionId);
+    if (seccion) {
+      seccion.scrollIntoView({ behavior: "smooth" });
+      history.pushState({ seccionId: seccionId }, "", "#" + seccionId); // Utiliza pushState en lugar de replaceState
+    }
   }
-}
-
-function handleClick(event) {
-  event.preventDefault();
-  let seccionId = event.target.getAttribute("href").substring(1);
-  cambiarSeccion(seccionId);
-}
-
-let enlacesNavegacion = document.querySelectorAll('a[href^="#"]');
-for (let i = 0; i < enlacesNavegacion.length; i++) {
-  enlacesNavegacion[i].addEventListener("click", handleClick);
-}
-
-window.addEventListener("popstate", function (event) {
-  if (event.state && event.state.seccionId) {
-    cambiarSeccion(event.state.seccionId);
+  
+  function handleClick(event) {
+    event.preventDefault();
+    let seccionId = event.target.getAttribute("href").substring(1);
+    cambiarSeccion(seccionId);
   }
-});
-
-// Deshabilitar la restauración automática de desplazamiento
-if ("scrollRestoration" in history) {
-  history.scrollRestoration = "manual";
-}
-
-// Función para manejar el evento de antes de descargar la página
-window.addEventListener("beforeunload", function () {
-  // Volver a la parte superior de la página al salir
-  window.scrollTo({ top: 0 });
-});
-
-// Función para manejar el evento de clic en el botón de atrás del navegador
-window.addEventListener("popstate", function (event) {
+  
+  let enlacesNavegacion = document.querySelectorAll('a[href^="#"]');
+  for (let i = 0; i < enlacesNavegacion.length; i++) {
+    enlacesNavegacion[i].addEventListener("click", handleClick);
+  }
+  
+  window.addEventListener("popstate", function (event) {
     if (event.state && event.state.seccionId) {
-      var seccionActual = document.getElementById(event.state.seccionId);
-      if (seccionActual && window.pageYOffset > 0) {
-        // Si no está en la parte superior de la página, desplázate hacia arriba
-        window.scrollTo({ top: 0, behavior: "smooth" });
-      } else {
-        // Si está en la parte superior de la página, vuelve atrás en el historial
-        window.history.back();
-      }
+      cambiarSeccion(event.state.seccionId);
+    } else {
+      window.scrollTo({ top: 0, behavior: "smooth" }); // Si no hay un estado, desplázate hacia arriba
     }
   });
+  
+  // Deshabilitar la restauración automática de desplazamiento
+  if ("scrollRestoration" in history) {
+    history.scrollRestoration = "manual";
+  }
+  
+  // Función para manejar el evento de antes de descargar la página
+  window.addEventListener("beforeunload", function () {
+    // Volver a la parte superior de la página al salir
+    window.scrollTo({ top: 0 });
+  });
+  
