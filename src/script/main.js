3e87a63ea551e2e4947a93a8262580e072510fdb
Nicolas Oviedo <nicoohdeoviedo@gmail.com>
2023-06-06 15:32:50 -0300

    se agrego funcionalidad de javascript

--- src/script/main.js
+++ src/script/main.js
@@ -10,3 +10,29 @@ const btn_menuAbrir = () =>{
 const btn_menuCerrar = () =>{
     navbar.classList.remove ("active")
 }
+
+
+function cambiarSeccion(seccionId) {
+    let seccion = document.getElementById(seccionId);
+    if (seccion) {
+        seccion.scrollIntoView({ behavior: 'smooth' });
+        history.replaceState({ seccionId: seccionId }, '', '#' + seccionId);
+    }
+}
+
+function handleClick(event) {
+    event.preventDefault();
+    let seccionId = event.target.getAttribute('href').substring(1);
+    cambiarSeccion(seccionId);
+}
+
+let enlacesNavegacion = document.querySelectorAll('a[href^="#"]');
+    for (let i = 0; i < enlacesNavegacion.length; i++) {
+    enlacesNavegacion[i].addEventListener('click', handleClick);
+}
+
+window.addEventListener('popstate', function(event) {
+    if (event.state && event.state.seccionId) {
+        cambiarSeccion(event.state.seccionId);
+    }
+});
